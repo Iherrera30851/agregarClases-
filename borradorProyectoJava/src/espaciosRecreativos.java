31e126ae451d3db875ecc61fb0270c5a61953442
Isaac Herrera <iherrera30851@ufide.ac.cr>
2025-07-23 19:30:14 -0600

    Add files via upload

---
+++ borradorProyectoJava/src/espaciosRecreativos.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author isaacherrera
+ */
+public class espaciosRecreativos {
+    
+    private int capacidadMaxima = 20;
+    private int capacidadActual = 0;
+
+    public boolean registrarEntrada(Socio socio) {
+        if (!socio.isMembresiaSocio()) {
+            JOptionPane.showMessageDialog(null, "Debe tener membresía activa para ingresar.");
+            return false;
+        }
+        if (capacidadActual >= capacidadMaxima) {
+            JOptionPane.showMessageDialog(null, "Espacio recreativo lleno.");
+            return false;
+        }
+        capacidadActual++;
+        JOptionPane.showMessageDialog(null, "Ingreso registrado. Personas dentro: " + capacidadActual);
+        return true;
+    }
+
+    public boolean registrarSalida(Socio socio) {
+        if (capacidadActual <= 0) {
+            JOptionPane.showMessageDialog(null, "El área está vacía.");
+            return false;
+        }
+        capacidadActual--;
+        JOptionPane.showMessageDialog(null, "Salida registrada. Personas restantes: " + capacidadActual);
+        return true;
+    }
+
+    public void verCapacidad() {
+        JOptionPane.showMessageDialog(null, "Personas en espacios recreativos: " + capacidadActual + " / " + capacidadMaxima);
+    }
+}
+
